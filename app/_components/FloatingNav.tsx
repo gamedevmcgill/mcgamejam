d9d0691e58562d4cfe635ac3bf97d3562b7a2ebd
Emilie Chen <emilie.hongjun.chen@mail.mcgill.ca>
2023-10-31 16:49:00 -0400

    stuff

--- app/_components/FloatingNav.tsx
+++ app/_components/FloatingNav.tsx
@@ -11,7 +11,7 @@ const FloatingNavCover = () => {
                 Learn More
             </a>
             <div className="lt-sm:my-4 gt-sm:mx-2"/>
-            <a href={ticketUrl} className="inline-flex justify-center items-center mx-auto py-3 px-3 gt-lg:px-10 lt-sm:w-72 text-base font-medium text-center text-white rounded-lg bg-amber-900 hover:bg-amber-700 focus:bg-amber-700 animate-pulse">
+            <a href={ticketUrl} className="inline-flex justify-center items-center mx-auto py-3 px-3 gt-lg:px-10 lt-sm:w-72 text-base font-medium text-center text-white rounded-lg bg-amber-900 hover:bg-amber-950 focus:bg-amber-950 animate-pulse">
                 Buy Tickets
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
@@ -27,7 +27,7 @@ const FloatingNavContent = () => {
         <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 p-4 bg-white rounded-xl shadow-lg gt-sm:flex lt-sm:flex-col">
             <Countdown />
             <div className="my-2" />
-            <a href={ticketUrl} className="inline-flex justify-center items-center py-3 px-3 gt-lg:px-10 gt-lg:w-52 lt-sm:w-72 text-base font-medium text-center text-white rounded-lg bg-amber-900 hover:bg-amber-700 focus:bg-amber-700 animate-pulse">
+            <a href={ticketUrl} className="inline-flex justify-center items-center py-3 px-3 gt-lg:px-10 gt-lg:w-52 lt-sm:w-72 text-base font-medium text-center text-white rounded-lg bg-amber-900 hover:bg-amber-950 focus:bg-amber-950 animate-pulse">
                 Buy Tickets
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,4 +1,6 @@
 import "@/global.css";
+import React from "react";
+
 
 export default function RootLayout({
                                        children,
