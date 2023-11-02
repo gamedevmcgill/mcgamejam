2cb78dea475dec7f766c8916e2b235ccede0b8fe
Emilie Chen <emilie.hongjun.chen@mail.mcgill.ca>
2023-11-02 02:31:47 -0400

    language switch button hidden on mobile

--- app/[lang]/_components/LanguageSwitchButton.tsx
+++ app/[lang]/_components/LanguageSwitchButton.tsx
@@ -9,14 +9,14 @@ const LanguageSwitchButton = ({ params: { lang }, currentPath } : { params: { la
     const flag = lang === "en-US" ? flagFrance : flagUS;
     const oppositeLang = lang === "en-US" ? "fr-FR" : "en-US";
     return (
-        <>
+        <div className="lt-sm:hidden">
             <a
                 className="fixed bottom-6 left-8 h-16 w-16 rounded-full bg-white text-black flex items-center justify-center shadow-lg text-xl"
                 href={currentPath.replace("[lang]", oppositeLang)}
             >
                 {flag}
             </a>
-        </>
+        </div>
     );
 }
 
